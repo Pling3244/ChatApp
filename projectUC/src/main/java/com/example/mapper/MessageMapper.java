376ea17d394266125620da776ec70160861db6bf
Pling3244 <129703338+Pling3244@users.noreply.github.com>
2023-06-13 12:37:42 +0900

    Delete projectUC directory

--- projectUC/src/main/java/com/example/mapper/MessageMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.example.domain.MessageVO;
-
-public interface MessageMapper {
-
-	public int messageSendInlist(MessageVO to);
-}
--- projectUC/src/main/webapp/resources/sql/uc_image.sql
+++
@@ -1,9 +0,0 @@
-create table uc_image(
-        num int ,
-        fileName varchar2(100) not null,
-        uploadPath varchar2(200) not null,
-        uuid varchar2(100) not null ,
-        primary key (uuid),
-        foreign key (num) references uc_car(num)
-    );
-
